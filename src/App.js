d268f2133ab75d88b7e9a270981cb247c6217723
Miraculous-V <dusworld@gmail.com>
2020-03-27 12:15:11 +0100

    implemented firebase utils, included users authentication storage into firestore

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import HomePage  from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header.component';
 import SignInAndSingUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserprofileDocument } from './firebase/firebase.utils';
+
 
 class App extends React.Component {
   constructor(props){
@@ -18,10 +19,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 componentDidMount(){
 
-  this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-    this.setState({ currentUser: user });
+  this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+   if(userAuth){
+     const userRef = await createUserprofileDocument(userAuth);
+     userRef.onSnapshot(snapShot => {
+       this.setState({
+         createUser: {
+           id:snapShot.id,
+           ...snapShot.data()
+         }
+       });
+       console.log(this.state);
+     });
 
-    console.log(user);
+   }
+    this.setState({ currentUser: userAuth });
   });
 }
 
@@ -36,7 +48,7 @@ this.unsubscribeFromAuth();
          <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/sign-in' component={SignInAndSingUpPage} />
+          <Route path='/signin' component={SignInAndSingUpPage} />
   
          </Switch>
       </div>
